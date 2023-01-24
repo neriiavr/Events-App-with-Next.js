de24ed480f84e9a4a2c888b51e65c3f0ce52ab10
neriiavr <avrupalineriman@gmail.com>
2023-01-24 22:44:40 +0100

    initial commit

---
+++ src/pages/events/[cat]/[id].js
@@ -0,0 +1,10 @@
+const EventPage = () => {
+    return (
+        <div>
+            <h1>Our Single Event</h1>
+        </div>    
+    )
+}
+
+export default EventPage;
+
---
+++ src/pages/events/index.js
@@ -0,0 +1,27 @@
+const EventsPage = () => {
+
+    return (
+        <div>
+            <h1>Events</h1>
+            <div>
+            <a href=''>
+        <img />
+        <h2>Events in Berlin</h2>
+
+       </a>
+       <a href=''>
+        <img />
+        <h2>Events in San Francisco</h2>
+
+       </a>
+       <a href=''>
+        <img />
+        <h2>Events in Barcelona</h2>
+
+       </a>
+            </div>
+        </div>
+    )
+}
+
+export default EventsPage;
